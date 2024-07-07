94fbc0f0d50f2c4167e19713db47060c77e1b6ef
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-07-07 10:40:40 +0530

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,32 +1,26 @@
 import  pandas as pd
 import boto3
-import io, os
+import io
 
 def loading_data():
-    access_key = os.environ.get("AWS_ACCESS_KEY_ID")
-    secret_key = os.environ.get("AWS_SECRET_ACCESS_KEY")
-    #bucket_name = os.environ.get("Bucket_Name")
-    print("-acckey-----:",access_key, secret_key)
-    # Initialize the S3 client
-    s3 = boto3.client('s3', aws_access_key_id=access_key,
-                          aws_secret_access_key=secret_key,
-                          region_name='us-east-1')
-    
+    s3 = boto3.client('s3')
+
     bucket_name = 'deeplearning-mlops'
     file_key = 'scada_data.csv'
-    
+    df = None
     # Download the file from S3
     try:
         response = s3.get_object(Bucket=bucket_name, Key=file_key)
         eeg_specs_data = response['Body'].read()
-    
+
         # Read the downloaded file using Pandas
         df = pd.read_csv(io.BytesIO(eeg_specs_data))
-        print(df.head())
-    
+        df.index = pd.to_datetime(df.index)
+        # Now you can work with the DataFrame 'df'
+        print(df)
+        
     except Exception as e:
         print(f"Error downloading or reading file from S3: {e}")
-
         
     return df
 
