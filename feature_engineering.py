ac48cab9fab84b4e27ca845ed4fc319d686a979e
Sathwika2505 <142480146+Sathwika2505@users.noreply.github.com>
2024-07-08 10:06:21 +0530

    Update feature_engineering.py

--- feature_engineering.py
+++ feature_engineering.py
@@ -8,18 +8,6 @@ def feature_engineering():
 
     data = data_analysis()
     data = data.drop(columns=['Asset_ID', 'Asset_Type'],axis=1)
-    def remove_outliers(data,par):
-        z = np.abs(stats.zscore(data[par]))
-        a=np.where(z > 3)
-        for i in a[0]:
-            if i in data.index:
-                data=data.drop(index=i,inplace=True)
-        return data 
-    
-    for j in data.columns:
-        if is_numeric_dtype(data[j]): 
-            data = remove_outliers(data,j)
-    data = data.resample('D').mean().fillna(method='ffill')
     print(data.head())
     data.to_csv("cleaned_power_distribution_data.csv",index=True)
     return data
